7f715d179a2dd4da6d0b269546c139a11c149cc1
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-08 02:26:54 -0500

    This should be done now :thinking:

---
+++ src/RedCraftPE/task/Generate.php
@@ -0,0 +1,24 @@
+<?PHP
+
+namespace RedCraftPE\task;
+
+use RedCraftPE\Skyblock;
+use pocketmine\scheduler\Task;
+
+class Generate extends Task {
+
+  public function __construct($islands, $level, $interval, $sender) {
+
+    $this->islands = $islands;
+    $this->level = $level;
+    $this->interval = $interval;
+    $this->sender = $sender;
+  }
+
+  public function onRun(int $tick) : void {
+
+    Skyblock::getInstance()->generateIsland($this->islands, $this->level, $this->interval);
+    $this->sender->setImmobile(false);
+    return;
+  }
+}
