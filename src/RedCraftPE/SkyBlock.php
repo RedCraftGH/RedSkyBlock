f8cf485742fa89447e10518d16b3dda367d940b2
Red <36905383+RedCraftGH@users.noreply.github.com>
2018-11-19 18:48:40 -0500

    Fixed a /is setworld bug

--- src/RedCraftPE/SkyBlock.php
+++ src/RedCraftPE/SkyBlock.php
@@ -90,7 +90,7 @@ class SkyBlock extends PluginBase implements Listener {
     }
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
         $level = $this->getServer()->getLevelByName($this->cfg->get("SkyBlockWorld"));
-        if (!$level) {
+        if ($this->skyblock->get("SkyBlockWorld") === "") {
           if (!$args) {
             $sender->sendMessage(TextFormat::RED . "You need to set a SkyBlock world with '/is setworld' before any other commands will function!");
             return true;
