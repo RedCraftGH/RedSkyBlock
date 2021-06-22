d7891283562519f93392b96933a8b534da9314ef
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-06-22 22:25:05 +0530

    Update Help.php
    Fixed /is help crash

--- src/RedCraftPE/RedSkyBlock/Commands/SubCommands/Help.php
+++ src/RedCraftPE/RedSkyBlock/Commands/SubCommands/Help.php
@@ -22,7 +22,7 @@ class Help {
         return true;
       } else {
 
-        $page = intval($args[1]);
+        $page = intval($args[2]);
         if ($page === 1) {
 
           $sender->sendMessage(TextFormat::AQUA . "SkyBlock Help Page {$page}: \n" . TextFormat::WHITE . "/is add <player>: Add a player to your island \n" . "/is ban <player>: Ban a player from your island \n" . "/is banned: See who is banned from your island \n" . "/is create: Create an island \n" . "/is createworld <world name>: Create a SkyBlock world");
