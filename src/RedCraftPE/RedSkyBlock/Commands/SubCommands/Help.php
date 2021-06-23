e709d89a5464b005465aad4c5694dbc2afb5e6b6
MrNinja008 <79526396+MrNinja008@users.noreply.github.com>
2021-06-23 22:30:37 +0530

    Fixed /is help crash

--- src/RedCraftPE/RedSkyBlock/Commands/SubCommands/Help.php
+++ src/RedCraftPE/RedSkyBlock/Commands/SubCommands/Help.php
@@ -16,13 +16,13 @@ class Help {
 
     if ($sender->hasPermission("redskyblock.help")) {
 
-      if (count($args) < 1) {
+      if (count($args) < 2) {
 
         $sender->sendMessage(TextFormat::WHITE . "Usage: /is help <page#>");
         return true;
       } else {
 
-        $page = intval($args[2]);
+        $page = intval($args[1]);
         if ($page === 1) {
 
           $sender->sendMessage(TextFormat::AQUA . "SkyBlock Help Page {$page}: \n" . TextFormat::WHITE . "/is add <player>: Add a player to your island \n" . "/is ban <player>: Ban a player from your island \n" . "/is banned: See who is banned from your island \n" . "/is create: Create an island \n" . "/is createworld <world name>: Create a SkyBlock world");
