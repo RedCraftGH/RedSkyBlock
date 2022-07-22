870a01d98486b80d5fae96fa383471cfc1e2930a
Red <36905383+RedCraftGH@users.noreply.github.com>
2022-07-22 17:31:04 -0400

    Update MessageConstructor.php
    Added function to update any messages not set to default -- adds any new messages to preexisting messages file without resetting any customized messages

--- src/RedCraftPE/RedSkyBlock/Utils/MessageConstructor.php
+++ src/RedCraftPE/RedSkyBlock/Utils/MessageConstructor.php
@@ -12,11 +12,14 @@ class MessageConstructor {
   public static $instance;
   public $plugin;
 
+  private $messageIds = [];
+
   public function __construct(SkyBlock $plugin) {
 
     $this->plugin = $plugin;
     $this->messages = $plugin->messages;
     self::$instance = $this;
+    $this->updateMessages();
   }
 
   public static function getInstance(): MessageConstructor {
@@ -54,5 +57,30 @@ class MessageConstructor {
     $message = str_replace("{ITALIC}", TextFormat::ITALIC, $message);
     $message = str_replace("{RESET}", TextFormat::RESET, $message);
     return $message;
+  }
+
+  public function updateMessages(): void {
+
+    $realString = file_get_contents($this->plugin->getDataFolder() . "../RedSkyBlock/messages.yml");
+    $realArray = yaml_parse($realString);
+    $realString = substr($realString, 0, -3);
+    $realKeys = array_keys($realArray);
+
+    $reference = yaml_parse(stream_get_contents($this->plugin->getResource("messages.yml")));
+    $referenceKeys = array_keys($reference);
+
+    $compare = array_diff($referenceKeys, $realKeys);
+
+    if (count($compare) > 0) {
+
+      foreach ($compare as $key) {
+
+        $realString .= $key . ": " . "\"" . $reference[$key] . "\"\n";
+      }
+      $realString .= "\n---";
+
+      file_put_contents($this->plugin->getDataFolder() . "../RedSkyBlock/messages.yml", $realString);
+      $this->plugin->messages->reload();
+    }
   }
 }
